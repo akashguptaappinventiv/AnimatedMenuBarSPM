add3ffe7e366abf797a339008ae9029d104b00a4
Akash <akash.gupta1@appinventiv.com>
2024-01-29 17:05:49 +0530

    package script updated

--- Package.swift
+++ Package.swift
@@ -3,6 +3,24 @@
 
 import PackageDescription
 
+//let package = Package(
+//    name: "MyLibrary",
+//    platforms: [
+//        .iOS(.v14),
+//        .macOS(.v11)
+//    ],
+//    products: [
+//        // Products define the executables and libraries a package produces, making them visible to other packages.
+//        .library(
+//            name: "MyLibrary",
+//            targets: ["MyLibrary"]),
+//    ],
+//    dependencies: [
+//        // Dependencies declare other packages that this package depends on.
+//        .package(url: "https://github.com/akashguptaappinventiv/AnimatedMenuBarSPM.git", from: "1.0.0")
+//        ]
+//)
+
 let package = Package(
     name: "MyLibrary",
     platforms: [
@@ -18,5 +36,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/akashguptaappinventiv/AnimatedMenuBarSPM.git", from: "1.0.0")
+        ],
+    targets: [
+            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+            // Targets can depend on other targets in this package, and on products in packages this package depends on.
+            .target(
+                name: "MyLibrary",
+                dependencies: []),
+            .testTarget(
+                name: "MyLibraryTests",
+                dependencies: ["MyLibrary"]),
         ]
 )
