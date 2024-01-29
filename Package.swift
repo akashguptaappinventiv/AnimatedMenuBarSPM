0f09b6a33836a97ea27f153c5031e7bacca0ab44
Akash <akash.gupta1@appinventiv.com>
2024-01-29 17:12:56 +0530

    updated script

--- Package.swift
+++ Package.swift
@@ -35,7 +35,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/akashguptaappinventiv/AnimatedMenuBarSPM.git", from: "1.0.0")
+//        .package(url: "https://github.com/akashguptaappinventiv/AnimatedMenuBarSPM.git", from: "1.0.0")
+        .package(url: "https://github.com/akashguptaappinventiv/AnimatedMenuBarSPM.git", branch: "main")
         ],
     targets: [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
